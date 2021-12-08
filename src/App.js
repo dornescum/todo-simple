403fad43e76c09edf436f9f54e7e4a71f3cba511
Mihai Dornescu <dornescu@gmail.com>
2021-12-08 17:21:35 +0200

    usable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
 
 
 function App() {
 	const [todos, setTodos] = useState([]);
 	const [todo, setTodo] = useState('');
+	const [todoEditing, setTodoEditing] = useState(null) ;
+	const [editingText, setEditingText] = useState('');
+
 
 
 	const handleSubmit = (e) => {
@@ -36,23 +39,63 @@ function App() {
 		setTodos(updatedTodos);
 	};
 
+	const editTodo=(id)=>{
+		 const updatedTodos = [...todos].map((todo)=>{
+			 if (todo.id === id){
+				 todo.text = editingText
+			 }
+			 return todo // il editez doar pe cel care are id ul corect
+		 })
+		setTodos(updatedTodos);
+		setTodoEditing(null)
+		setEditingText('')
+	}
+
+	useEffect(()=>{
+		const temp = localStorage.getItem('todos');
+		const loadedTodos = JSON.parse(temp);
+		if (loadedTodos){
+			setTodos(loadedTodos)
+		}
+	}, [])
+
+	useEffect(()=>{
+		const temp = JSON.stringify(todos);
+		localStorage.setItem('todos', temp)
+	}, [todos])
+
+
 	return (
 		<div className="App">
-			<form action="" onSubmit={handleSubmit}>
-				<input type="text" onChange={(e) => {
-					setTodo(e.target.value);
-				}} value={todo}/>
-				<button type="submit">Add todo</button>
-			</form>
+			<Form />
+			{/*<form action="" onSubmit={handleSubmit}>*/}
+			{/*	<input type="text" onChange={(e) => {*/}
+			{/*		setTodo(e.target.value);*/}
+			{/*	}} value={todo}/>*/}
+			{/*	<button type="submit">Add todo</button>*/}
+			{/*</form>*/}
 
 			{todos.map((todo) => {
 				return <div key={todo.id}>
-					<div>
-						{todo.text}
-					</div>
+					{todoEditing === todo.id ? (
+						<input type="text" onChange={(e)=>setEditingText(e.target.value)} value={editingText }/>
+					): (
+						<div>
+							{todo.text}
+						</div>
+					)}
+
 					<button onClick={() => deleteTodo(todo.id)}>delete</button>
+
 					<input type="checkbox" onChange={() => toggleComplete(todo.id)} checked={todo.completed}/>
-					<button> edit todo</button>
+
+					{todoEditing === todo.id ? (
+						<button onClick={()=>{editTodo(todo.id)}}> submit edit </button>
+					):(
+						<button onClick={()=>{setTodoEditing(todo.id)}}> edit todo</button>
+					)}
+
+
 				</div>;
 			})}
 		</div>
@@ -60,3 +103,12 @@ function App() {
 }
 
 export default App;
+
+// const Form =({handleSubmit, todo, setTodo})=>{
+// 	return <form action="" onSubmit={handleSubmit}>
+// 		<input type="text" onChange={(e) => {
+// 			setTodo(e.target.value);
+// 		}} value={todo}/>
+// 		<button type="submit">Add todo</button>
+// 	</form>
+// }
